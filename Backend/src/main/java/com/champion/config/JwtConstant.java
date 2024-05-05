44e905dda52600a9a58e9967a4309092dda5094b
pravin pralhad patil <113687095+2001pravin@users.noreply.github.com>
2024-05-05 14:32:10 +0530

    E

--- Backend/src/main/java/com/champion/config/JwtConstant.java
+++
@@ -1,9 +0,0 @@
-package com.champion.config;
-
-public class JwtConstant {
-	public static final String SECRET_KEY="buhdyfbhrhbbhbksmsjdmdmdcbhccbjcbksnoppsjbohbgfdf";
-	public static final String JWT_HEADER="Authorization";
-		
-	
-
-}
